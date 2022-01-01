15d23f538d79c5f1d0ebf7e8477e948df71c5556
vuralkurt <vuralkurt3535@gmail.com>
2022-01-01 11:50:40 +0100

    de

--- src/denemelerim/asd.java
+++ src/denemelerim/asd.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class asd {
 
     public static void main(String[] args) {
-
+            //
         List<String> liste=new ArrayList<String>();
         liste.add("ali");
         liste.add("veli");
